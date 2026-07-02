6a731e85172cc27333af5b8339483c65f5ca77a3
Soumyajitttt <soumyajitchaudhury4@gmail.com>
2026-07-02 16:12:10 +0530

    fix: correct socket.io import and enhance server setup in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,30 @@
 import express from 'express';
 import http from 'http';
-import socket from 'socket.io';
+import {Server} from 'socket.io';
 import mongoose from 'mongoose';
 import { Chess } from 'chess.js';
+import path from 'path';
+import { title } from 'process';
 
-const app = exrpress();
+const app = express();
 const server = http.createServer(app);
-const io = socket(server);
-
+const io = new Server(server);
 
 const chess = new Chess();
+let players = {};
+let currentTurn = 'white'; 
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index', {title: 'Chess Game'});
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected: ' + socket.id);
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
