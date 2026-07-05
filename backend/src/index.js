97d1699abc707e080286f680c8a6ae0c9c93176b
Soumyajitttt <soumyajitchaudhury4@gmail.com>
2026-07-06 03:46:24 +0530

    feat: add EJS view engine support and create game.js for frontend logic

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import http from 'http';
-import {Server} from 'socket.io';
+import { Server } from 'socket.io';
 import mongoose from 'mongoose';
 import { Chess } from 'chess.js';
 import path from 'path';
-import { title } from 'process';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
@@ -12,13 +15,14 @@ const io = new Server(server);
 
 const chess = new Chess();
 let players = {};
-let currentTurn = 'white'; 
+let currentTurn = 'white';
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Chess Game'});
+    res.render('index', { title: 'Chess Game' });
 });
 
 io.on('connection', (socket) => {
