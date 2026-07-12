f21401c05e865f3f4ff6a65ed68851024ecd6a7c
Soumyajitttt <soumyajitchaudhury4@gmail.com>
2026-07-13 00:57:31 +0530

    feat: implement chess board rendering and piece setup in game.js

--- backend/src/public/js/game.js
+++ backend/src/public/js/game.js
@@ -1,1 +1,37 @@
 const socket = io();
+const chess = new Chess();
+const boardElement = document.querySelector('.board');
+
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderBoard = () => {
+    const board = chess.board();
+    boardElement.innerHTML = ''
+    board.forEach((row, rowIndex) => {
+        row.forEach((square, squareIndex) => {
+            const squareElement = document.createElement('div');
+            squareElement.classList.add('square',
+            (rowIndex + squareIndex) % 2 === 0 ? 'bg-amber-100' : 'bg-amber-800');
+
+            squareElement.dataset.row = rowIndex;
+            squareElement.dataset.col = squareIndex;
+
+            if (square) {
+                const pieceElement = document.createElement('div');
+                pieceElement.classList.add('piece',square.color === 'white' ? 'white' : 'black');
+                pieceElement.innerText = getPieceUnicode(square);
+                pieceElement.draggable = true;
+                pieceElement.dataset.piece = square.type;
+                pieceElement.dataset.color = square.color;
+                squareElement.appendChild(pieceElement);
+            }
+            boardElement.appendChild(squareElement);
+        });
+    });
+};
+
+const handleMove = () =>{};
+
+const getPieceUnicode = () => {};
