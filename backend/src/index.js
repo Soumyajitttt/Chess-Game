8b5570647fc1912706b594f6c1281b6633c540fd
Soumyajitttt <soumyajitchaudhury4@gmail.com>
2026-07-15 15:41:48 +0530

    fix: update server listen logic to use environment variable for port

--- backend/src/index.js
+++ backend/src/index.js
@@ -76,6 +76,7 @@ io.on('connection', (socketio) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
